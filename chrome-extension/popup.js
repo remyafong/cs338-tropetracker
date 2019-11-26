34b4086b603e412f4f4dcc8323568c75b9f6cac0
pervezn <nidapervez2020@u.northwestern.edu>
2019-11-26 12:53:01 -0600

    chrome extension basics

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,8 +1,45 @@
-let changeColor = document.getElementById('findTrope');
+const bkg = chrome.extension.getBackgroundPage();
+let trope = ['Catch 22', 'Not all that Glitters is Gold', 'David and Goliath']
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+//gets link from the browser
+let getLink = () => {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+   bkg.console.log("URL IS: ", url)
+  });
+}
+
+
+//display tropes  --> don't know if this works
+let displayTropes = (foundTropesList) => {
+  var toAdd = document.getElementById('links');
+    for(let i = 0; i < foundTropesList.length; i++){
+      var newDiv = document.createElement('div');
+      newDiv.innerText = foundTropesList[i];
+      newDiv.className = 'tropes';
+      toAdd.appendChild(newDiv);
+    }
+    document.body.appendChild(toAdd);
+    document.getElementById('goToTT').style.visibility = 'visible'
+}
+
+
+// event Listener for Button
+document.addEventListener('DOMContentLoaded', function() {
+  var submit = document.getElementById('submit');
+  var goToTT = document.getElementById('goToTT');
+  // onClick's logic below:
+  submit.addEventListener('click', function() {
+      getLink();
+      //pull data from database
+      //store in variable
+      displayTropes(trope) //pass variable instead of trope
+      this.disabled = true;
   });
 
-  
+  goToTT.addEventListener('click', () => {
+    
+  })
+
+});
+
